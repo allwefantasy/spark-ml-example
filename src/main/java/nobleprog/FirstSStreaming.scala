0f382212c57d9018f1994373f4c5e2503f74ec99
WilliamZhu <allwefantasy@gmail.com>
2017-09-07 10:51:05 +0800

    add more example

--- src/main/java/nobleprog/FirstSStreaming.scala
+++ src/main/java/nobleprog/FirstSStreaming.scala
@@ -1,7 +1,7 @@
 package nobleprog
 
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{ForeachWriter, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
@@ -21,8 +21,27 @@ object FirstSStreaming {
       .option("port", 9005)
       .load()
 
-    val query = ss.as[String].flatMap(_.split(" ")).writeStream
-      .outputMode("append")
+    val writeStream = ss.as[String].flatMap(_.split(" ")).writeStream
+
+    writeStream.foreach(new ForeachWriter[String] {
+      override def process(value: String): Unit = {
+        //对分区的每个处理
+      }
+
+      override def close(errorOrNull: Throwable): Unit = {
+        //不保证一定被调用
+      }
+
+      override def open(partitionId: Long, version: Long): Boolean = {
+        //每个 partition 即每个 task 会在开始时调用此 open() 方法,你可以通过版本version字段判定这个是不是需要重做
+        //虽然SS保证内部的幂等性，但是只要有数据同步到外部，就会存在多次写入的问题。如果你通过version和partitionId 已经发现内容
+        //被写过了，则可以返回false 跳过这次写入
+
+        true
+      }
+    })
+
+    val query = writeStream.outputMode("append")
       .format("console")
       .start()
 
